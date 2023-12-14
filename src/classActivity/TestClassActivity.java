509dede481b1b9c7290e5dad103afbc391f1c0e3
ewivanhunt <122378385+ewivanhunt@users.noreply.github.com>
2023-12-13 19:30:25 -0500

    Update TestClassActivity.java

--- src/classActivity/TestClassActivity.java
+++ src/classActivity/TestClassActivity.java
@@ -6,7 +6,6 @@ public class TestClassActivity {
         Ethan name1 = new Ethan("Ethan", 22, "Oldest of 4");
         Drive car1 = new Drive("Toyota", "Camry", 2001);
 
-        // Call methods from each class
         System.out.println("Information:");
         name1.EthanInfo();
 
